778fdd97783507fecde2638fbb584b4906d817ea
Steven Anderson <steve@whilefalse.net>
2009-09-02 10:31:43 +0200

    Tidied up and robustified some more

--- pymarple.py
+++ pymarple.py
@@ -9,12 +9,16 @@ def find(postcode):
     resource = "%s?%s" % (url, urllib.urlencode({'p':postcode,'f':'csv'}))
     
     try:
+        #Get location
         resource = urllib.urlopen(resource)
-    except IOError:
-        raise PostcodeNotFound()    
-
-    if resource.info()['Content-Type'] != "text/csv":
+        #Make sure it's in csv format. If it's not, we were redirected to home page
+        assert resource.info()['Content-Type'] == "text/csv"
+        #Split it on commas
+        values = resource.read().split(',')
+        #Make sure it's in the format postcode, lat, long
+        assert len(values) == 3
+        #Return the lat and long
+        return [float(v) for v in values[1:]]
+    except (IOError, AssertionError, ValueError):
         raise PostcodeNotFound()
-
-    return [float(x) for x in resource.read().split(',')[1:]]
     
